7b78e3547dcbcee1f619d60d1aa1b57501aa4978
vadimcvirko <promiti90@gmail.com>
2023-01-30 20:28:18 +0300

    theme interface children

--- src/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,13 +1,14 @@
-import React, {FC, useMemo, useState} from 'react';
+import React, {FC, ReactNode, useMemo, useState} from 'react';
 import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "../lib/ThemeContext";
 
 const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT;
 
+interface ThemeProviderProps {
+    children?: ReactNode
+}
 
-// @ts-ignore
-const ThemeProvider: FC = ({children}) => {
+const ThemeProvider = ({children}: ThemeProviderProps) => {
     const [theme, setTheme] = useState<Theme>(defaultTheme);
-
 
 
     const defaultProps = useMemo(() => ({
@@ -16,7 +17,7 @@ const ThemeProvider: FC = ({children}) => {
     }), [theme])
 
     return (
-            <ThemeContext.Provider value={defaultProps}>
+        <ThemeContext.Provider value={defaultProps}>
             {children}
         </ThemeContext.Provider>
     );
